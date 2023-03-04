7860170d715f6ef559f48168dc1322c6ebcb3674
Andrew Loeliger <loeliger@wisc.edu>
2023-03-04 03:40:35 -0600

    Re organize crab configs

--- Ntuplizer/python/crabConfigs/crabConfig_ZB2018D.py
+++ Ntuplizer/python/crabConfigs/crabConfig_ZB2018D.py
@@ -1,20 +1,25 @@
 import CRABClient
 from CRABClient.UserUtilities import config
+import os
+
 config = config()
 
-config.General.requestName = 'unsupervisedZToeeAnalysis_Feb2023_data'
-config.General.workArea = 'crab_projects'
+config.General.requestName = 'unsupervisedZToeeAnalysis_26Feb2023_ZB2018D'
+config.General.workArea = 'crabWorkArea'
 config.General.transferOutputs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'ntuplize.py'
+config.JobType.psetName = f'{os.environ["CMSSW_BASE"]}/src/unsupervisedZToeeHunt/Ntuplizer/python/ntuplize.py'
+config.JobType.maxMemoryMB = 4000
 
 config.Data.inputDataset = '/ZeroBias/Run2018D-UL2018_MiniAODv2-v1/MINIAOD'
 config.Data.secondaryInputDataset = '/ZeroBias/Run2018D-v1/RAW'
 config.Data.inputDBS = 'global'
+#config.Data.splitting = 'FileBased'
 config.Data.splitting = 'Automatic'
+config.Data.unitsPerJob = 1080
 config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
 config.Data.publication = False
-config.Data.outputDatasetTag = 'unsupervisedZToeeAnalysis_Feb2023_data'
+config.Data.outputDatasetTag = 'unsupervisedZToeeAnalysis_26Feb2023_ZB2018D'
 
 config.Site.storageSite = 'T2_US_Wisconsin'
--- Ntuplizer/python/crabConfigs/crabConfig_singleFileTest.py
+++ Ntuplizer/python/crabConfigs/crabConfig_singleFileTest.py
@@ -3,18 +3,19 @@ from CRABClient.UserUtilities import config
 config = config()
 
 config.General.requestName = 'unsupervisedZToeeAnalysis_Feb2023_data_test'
-config.General.workArea = 'crab_projects'
+config.General.workArea = 'crabWorkArea'
 config.General.transferOutputs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'ntuplize.py'
+config.JobType.psetName = '/afs/hep.wisc.edu/cms/aloeliger/anomalyTriggerWork/CMSSW_12_4_3/src/unsupervisedZToeeHunt/Ntuplizer/python/ntuplize.py'
+config.JobType.maxMemoryMB = 4000
 
 config.Data.inputDataset = '/ZeroBias/Run2018D-UL2018_MiniAODv2-v1/MINIAOD'
 config.Data.secondaryInputDataset = '/ZeroBias/Run2018D-v1/RAW'
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
+config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 1
-config.Data.totalUnits = 1
+config.Data.totalUnits = 5
 config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
 config.Data.publication = False
 config.Data.outputDatasetTag = 'unsupervisedZToeeAnalysis_Feb2023_data_test'
